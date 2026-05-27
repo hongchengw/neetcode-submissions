ea226ad857c7d2bc1d8c80d00b5985033e4355d6
Hong Cheng Wang <165329338+hongchengw@users.noreply.github.com>
2026-05-26 22:49:22 -0400

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums: number[]): number[] {
+        let ans: number[] = [];
+        for(let i = 0; i < 2; ++i) {
+            for(let j = 0; j < nums.length; ++j) {
+                ans.push(nums[j]);
+            }
+        }
+
+        return ans;
+    }
+}
